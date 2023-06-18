f4cf16cf180bcd48965818a5ee20575eacbe5714
mahanteshKT <mahanteshkt123@gmail.com>
2023-06-18 20:40:30 +0530

    djangojs update

--- Music_sharing_portal/settings.py
+++ Music_sharing_portal/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-dsp@obn_4ld3b1-!&p(^1_!e22kicvv6xw!kvk+ufmbo%kn&wh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*','127.0.0.1']
 
