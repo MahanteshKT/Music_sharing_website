2e5b2a4e3f4dd040cd1fd62de597d64d34988d9d
mahanteshKT <mahanteshkt123@gmail.com>
2023-06-18 17:07:01 +0530

    django update

--- Music_sharing_portal/settings.py
+++ Music_sharing_portal/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-dsp@obn_4ld3b1-!&p(^1_!e22kicvv6xw!kvk+ufmbo%kn&wh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','192.168.1.2','192.168.222.201']
+ALLOWED_HOSTS = ['*','127.0.0.1','192.168.1.2','192.168.222.201']
 
 
 AUTHENTICATION_BACKENDS = [
